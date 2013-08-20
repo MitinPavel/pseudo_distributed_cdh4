09d410f7c888b8d2392ca30a1f0d30ec03b157a2
pm <mitin.pavel@gmail.com>
2013-08-20 19:59:17 +0300

    Download and install CDH4 package.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,13 @@
-bash "test_bash" do
-  user "vagrant"
-  cwd "/home/vagrant"
-  code <<-CODE
-    touch blah 
-  CODE
+# http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb
+
+execute 'download CDH4 package' do
+  command 'wget http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb'
+  cwd '/tmp'
+  action :run
 end
+
+dpkg_package 'cdh4-repository' do
+  source '/tmp/cdh4-repository_1.0_all.deb'
+  action :install
+end
+
