d3e448714846950667cd68df2e6761de923c9d82
pm <mitin.pavel@gmail.com>
2013-08-20 19:30:10 +0300

    Introduce metadata file and license.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       'Pavel Mitin'
+maintainer_email 'mitin.pavel@gmail.com'
+license          'MIT'
+description      "Installs and configures CDH4 with YARN on a Single Linux Node in Pseudo-distributed mode"
+version          "0.0.1"
+
