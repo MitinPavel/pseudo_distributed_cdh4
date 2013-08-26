50e2ce3a5dad768e5266c1036516f422228a5188
pm <mitin.pavel@gmail.com>
2013-08-26 20:05:10 +0300

    Make "start hdfs <node>" idempotent.

--- recipes/default.rb
+++ recipes/default.rb
@@ -50,9 +50,14 @@ end
     command "service hadoop-hdfs-#{node} start"
     user "root"
     action :run
-    #only_if { `pidof hadoop-hdfs-#{node}` == '' }
+    only_if { `pgrep -u #{hdfs_user} -f hadoop-hdfs-#{node}` == '' }
   end
 end
+#vagrant@precise64:~$ sudo jps
+#9022 NameNode
+#9436 Jps
+#9121 SecondaryNameNode
+#8935 DataNode
 
 execute "create tmp dir" do
   command "sudo -u #{hdfs_user} hadoop fs -mkdir /tmp && sudo -u #{hdfs_user} hadoop fs -chmod -R 1777 /tmp"
